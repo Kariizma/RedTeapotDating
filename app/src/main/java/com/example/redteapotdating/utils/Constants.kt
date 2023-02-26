de3ed9c8a0fa6cc8f1455ecb8c95e6040f0a59d4
Karan Vadhan <karanvadhan789@gmail.com>
2023-02-26 16:43:26 -0500

    Update Constants.kt

--- app/src/main/java/com/example/redteapotdating/utils/Constants.kt
+++ app/src/main/java/com/example/redteapotdating/utils/Constants.kt
@@ -7,15 +7,11 @@ class Constants {
         /**
          * I tried pulling the API data from the hinge url but i had too many problems pulling from there
          *  - Data would not populate my users list
-         *  - API call would just some 100000 ms error
          *  - needed to have another Data class of Users for a List of User for the Users data
          *  - tried to access with a http://
          *      - error: Failure: CLEARTEXT communication to hinge-ue1-dev-cli-android-homework.s3-website-us-east-1.amazonaws.com not permitted by network security policy
          *  - tried to access with a https://
          *      - no response from the Data and when i put it into a web broswer i cant connect to the link
-         *
-         *  if needed i can upload that code aswell so i can gets some good feedback and learn from it.
-         *  If i had more time i would try to find a fix for this, sorry :(
          *
          *
          *  i just created a new API with just the list of the users data instead.
